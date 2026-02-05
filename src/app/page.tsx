42272d493141d544873054783a829b2f04dce0d0
Shi Tao Luo <sluo5991@gmail.com>
2026-02-05 14:40:25 -0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+    </div>
+  );
+}
