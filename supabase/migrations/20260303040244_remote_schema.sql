deb207c652f51d224d85538e8b6b39f750be2f8a
Shi Tao Luo <sluo5991@gmail.com>
2026-03-10 23:26:40 -0400

    update supabase migration to add with check for update policies

--- supabase/migrations/20260303040244_remote_schema.sql
+++
@@ -1,7 +0,0 @@
-create extension if not exists "hypopg" with schema "extensions";
-
-create extension if not exists "index_advisor" with schema "extensions";
-
-drop extension if exists "pg_net";
-
-
